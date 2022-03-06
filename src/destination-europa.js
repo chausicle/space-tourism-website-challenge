4d1a863cda5e79a59c4beeebdfa670faf433a066
Justin Chau <chausicle@gmail.com>
2022-03-06 12:08:01 -0800

    Initial mobile responsive for destination page

--- src/destination-europa.js
+++ src/destination-europa.js
@@ -1,14 +1,38 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const planetImg = document.querySelector('img.planet-image')
+document.addEventListener("DOMContentLoaded", () => {
+  const planetImg = document.querySelector("img.planet-image");
+  const navHamburger = document.querySelector("svg.nav-hamburger");
+  const navHamburgerClose = document.querySelector("svg.nav-hamburger-close");
+  navHamburger.addEventListener("click", openSideMenu);
+  navHamburgerClose.addEventListener("click", closeSideMenu);
 
   window.onresize = () => {
-    console.log(planetImg)
-    console.log(window.innerWidth)
+    console.log(planetImg);
+    console.log(window.innerWidth);
 
-    if (window.innerWidth <= 1080 && window.innerWidth >= 800) {
-      planetImg.src = "assets/destination/image-europa.webp"
+    if (window.innerWidth <= 1080) {
+      planetImg.src = "assets/destination/image-europa.webp";
     } else if (window.innerWidth > 1080) {
-      planetImg.src = "assets/destination/image-europa.png"
+      planetImg.src = "assets/destination/image-europa.png";
     }
-  }
-})
+  };
+});
+
+function openSideMenu(event) {
+  const sidemenu = document.querySelector("div.nav-side-menu");
+  const hamburgerClose = document.querySelector("svg.nav-hamburger-close");
+  const hamburger = document.querySelector("svg.nav-hamburger");
+
+  hamburger.classList.replace("show", "hide");
+  sidemenu.classList.replace("close-menu", "open-menu");
+  hamburgerClose.classList.replace("hide", "show");
+}
+
+function closeSideMenu(event) {
+  const sidemenu = document.querySelector("div.nav-side-menu");
+  const hamburgerClose = document.querySelector("svg.nav-hamburger-close");
+  const hamburger = document.querySelector("svg.nav-hamburger");
+
+  hamburger.classList.replace("hide", "show");
+  sidemenu.classList.replace("open-menu", "close-menu");
+  hamburgerClose.classList.replace("show", "hide");
+}
