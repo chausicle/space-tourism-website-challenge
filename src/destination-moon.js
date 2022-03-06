adede11e8433480e83d6d421fd8696f7ebbf2923
Justin Chau <chausicle@gmail.com>
2022-03-06 10:37:38 -0800

    Change planet image src from png to webp for tablet and mobile view

---
+++ src/destination-moon.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const planetImg = document.querySelector('img.planet-image')
+
+  window.onresize = () => {
+    console.log(planetImg)
+    console.log(window.innerWidth)
+
+    if (window.innerWidth <= 1080 && window.innerWidth >= 800) {
+      planetImg.src = "assets/destination/image-moon.webp"
+    } else if (window.innerWidth > 1080) {
+      planetImg.src = "assets/destination/image-moon.png"
+    }
+  }
+})
