15190532b32255981c5201a144b65e4e4ba1eeda
Justin Chau <chausicle@gmail.com>
2022-03-08 08:40:45 -0800

    Structure crew image and crew navbar depending on static view and view resize

--- src/crew-commander.js
+++ src/crew-commander.js
@@ -1,19 +1,63 @@
 document.addEventListener("DOMContentLoaded", () => {
   const crewImage = document.querySelector("img.crew-image");
+  const crewUpperContainer = document.querySelector("div.crew-upper-container");
+  const crewNavbar = document.querySelector("div.crew-navbar");
+  const crewHeadingTitle = document.querySelector("h4.crew-heading-title");
+  const mobileNavContainer = document.createElement("div");
+  const hr = document.createElement("hr");
+  mobileNavContainer.setAttribute("class", "mobile-nav");
+  hr.setAttribute("class", "mobile-hr");
 
   if (window.innerWidth > 1080) {
     crewImage.src = "assets/crew/image-douglas-hurley.png";
-  } else if (window.innerWidth <= 1080) {
+  } else if (window.innerWidth <= 1080 && window.innerWidth > 800) {
     crewImage.src = "assets/crew/image-douglas-hurley.webp";
+  } else if (window.innerWidth <= 800) {
+    crewImage.src = "assets/crew/image-douglas-hurley.webp";
+    mobileNavContainer.appendChild(crewImage);
+    mobileNavContainer.appendChild(hr);
+    mobileNavContainer.appendChild(crewNavbar);
+    crewUpperContainer.insertBefore(mobileNavContainer, crewHeadingTitle);
   }
+
+  window.onresize = onResize;
 });
 
-window.onresize = () => {
+function onResize() {
   const crewImage = document.querySelector("img.crew-image");
-  
+  const bottomContainer = document.querySelector("div.bottom-container");
+  const crewUpperContainer = document.querySelector("div.crew-upper-container");
+  const crewNavbar = document.querySelector("div.crew-navbar");
+  const crewHeadingTitle = document.querySelector("h4.crew-heading-title");
+  const mobileNavContainer = document.querySelector("div.mobile-nav");
+
   if (window.innerWidth > 1080) {
     crewImage.src = "assets/crew/image-douglas-hurley.png";
-  } else if (window.innerWidth <= 1080) {
+    if (mobileNavContainer) {
+      mobileNavContainer.remove();
+      crewUpperContainer.appendChild(crewNavbar);
+      bottomContainer.appendChild(crewImage);
+    }
+  } else if (window.innerWidth <= 1080 && window.innerWidth > 800) {
     crewImage.src = "assets/crew/image-douglas-hurley.webp";
+    if (mobileNavContainer) {
+      mobileNavContainer.remove();
+      crewUpperContainer.appendChild(crewNavbar);
+      bottomContainer.appendChild(crewImage);
+    }
+  } else if (window.innerWidth <= 800) {
+    crewImage.src = "assets/crew/image-douglas-hurley.webp";
+    if(!mobileNavContainer) {
+      const mobileNavContainer = document.createElement("div");
+      const hr = document.createElement("hr");
+      mobileNavContainer.setAttribute("class", "mobile-nav");
+      hr.setAttribute("class", "mobile-hr");
+      crewNavbar.remove()
+      crewImage.remove()
+      mobileNavContainer.appendChild(crewImage)
+      mobileNavContainer.appendChild(hr)
+      mobileNavContainer.appendChild(crewNavbar)
+      crewUpperContainer.insertBefore(mobileNavContainer, crewHeadingTitle);
+    }
   }
-};
+}
