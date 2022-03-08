1c360fd942ca274e8880143253433d71a8c8e565
Justin Chau <chausicle@gmail.com>
2022-03-08 11:55:45 -0800

    Mobile responsive for technology page

--- src/technology-vehicle.js
+++ src/technology-vehicle.js
@@ -1,11 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const techImage = document.querySelector("img.technology-image")
+  const techImage = document.querySelector("img.technology-image");
+  const navHamburger = document.querySelector("svg.nav-hamburger");
+  const navHamburgerClose = document.querySelector("svg.nav-hamburger-close");
+  navHamburger.addEventListener("click", openSideMenu);
+  navHamburgerClose.addEventListener("click", closeSideMenu);
 
-  if(window.innerWidth > 1080) {
-    techImage.src = "assets/technology/image-launch-vehicle-portrait.jpg"
-  } else if(window.innerWidth <= 1080 && window.innerWidth > 800) {
-    techImage.src = "assets/technology/image-launch-vehicle-landscape.jpg"
-  } else if(window.innerWidth <= 800) {
-    techImage.src = "assets/technology/image-launch-vehicle-landscape.jpg"
+  if (window.innerWidth > 1080) {
+    techImage.src = "assets/technology/image-launch-vehicle-portrait.jpg";
+  } else if (window.innerWidth <= 1080 && window.innerWidth > 800) {
+    techImage.src = "assets/technology/image-launch-vehicle-landscape.jpg";
+  } else if (window.innerWidth <= 800) {
+    techImage.src = "assets/technology/image-launch-vehicle-landscape.jpg";
   }
 });
+
+function openSideMenu(event) {
+  const sidemenu = document.querySelector("div.nav-side-menu");
+  const hamburgerClose = document.querySelector("svg.nav-hamburger-close");
+  const hamburger = document.querySelector("svg.nav-hamburger");
+
+  hamburger.classList.replace("show", "hide");
+  sidemenu.classList.replace("close-menu", "open-menu");
+  hamburgerClose.classList.replace("hide", "show");
+}
+
+function closeSideMenu(event) {
+  const sidemenu = document.querySelector("div.nav-side-menu");
+  const hamburgerClose = document.querySelector("svg.nav-hamburger-close");
+  const hamburger = document.querySelector("svg.nav-hamburger");
+
+  hamburger.classList.replace("hide", "show");
+  sidemenu.classList.replace("open-menu", "close-menu");
+  hamburgerClose.classList.replace("show", "hide");
+}
